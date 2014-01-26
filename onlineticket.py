e09ef70b88a95be57b42fa11694c0ff3a23d1c4e
Hagen Fritsch <fritsch+git@in.tum.de>
2014-01-26 16:22:26 +0100

    Add support for decoding RAWJSN data.

--- onlineticket.py
+++ onlineticket.py
@@ -329,6 +329,32 @@ class OT_U_TLAY(DataBlock):
                 ('fields', 0, None, read_fields)
              ]
 
+
+class OT_RAWJSN(DataBlock):
+    """A data block containing raw json data."""
+    def __init__(self, *args, **kwargs):
+        super(OT_RAWJSN, self).__init__(*args,**kwargs)
+        json_data = self.read(self.header['length'] - 12)
+        import json
+        try:
+          self.data.update(json.loads(json_data))
+        except:
+          # json is likely unhappy about keys missing quotes
+          # (e.g. {key: 'value'} instead of {'key': 'value'})
+          import yaml
+          try:
+            self.data.update(yaml.load(json_data))
+          except:
+            # yaml is likely unhappy about missing spaces after colons
+            # (e.g. {key:'value'} instead of {key: 'value'})
+            try:
+              with_spaces = re.sub(r'([,{][^}:]+?):([{[0-9\'"])', r'\1: \2', json_data)
+              self.data.update(yaml.load(with_spaces))
+            except:
+              print 'Couldn\'t decode JSON data', repr(json_data)
+              raise
+
+
 class OT(DataBlock):
     generic = [
         ('header', 3),
@@ -352,7 +378,8 @@ def read_block(data, offset):
                    '0080ID': OT_0080ID,
                    '0080BL': OT_0080BL,
                    '0080VU': OT_0080VU,
-                   '1180AI': OT_1180AI}
+                   '1180AI': OT_1180AI,
+                   'RAWJSN': OT_RAWJSN}
     block_type = data[offset:offset+6]
     return block_types.get(block_type, GenericBlock)(data, offset)
 
