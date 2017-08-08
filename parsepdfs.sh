b7b89f15b16ec14872025ce8ab2daa6b4ca78756
muelli <muelli@cryptobitch.de>
2017-08-08 19:40:55 +0200

    use "VERSION" instead of "V"

--- parsepdfs.sh
+++ parsepdfs.sh
@@ -2,8 +2,8 @@ ZX=$(dirname $0)
 CORE=$(echo $ZX/core*.jar)
 JAVASE=$(echo $ZX/javase*.jar)
 VERSION=3.1.0
-[ -e $JAVASE ] || wget http://repo1.maven.org/maven2/com/google/zxing/javase/$V/javase-$V.jar
-[ -e $CORE ] || wget http://repo1.maven.org/maven2/com/google/zxing/core/$V/core-$V.jar
+[ -e $JAVASE ] || wget http://repo1.maven.org/maven2/com/google/zxing/javase/$VERSION/javase-$VERSION.jar
+[ -e $CORE ] || wget http://repo1.maven.org/maven2/com/google/zxing/core/$VERSION/core-$VERSION.jar
 rename -v 'tr/ ()/___/' *
 for i in *.pdf; do pdfimages $i $i; done
 for i in *.pbm *.ppm; do convert $i $i.png; rm $i; done
