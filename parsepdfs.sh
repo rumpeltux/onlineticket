93e9dc8147d6e0e3a35225f60723c43add57ce65
Quinn <78063175+quinn-dev@users.noreply.github.com>
2021-09-05 14:49:54 +0000

    Use SSL to fetch zxing

--- parsepdfs.sh
+++ parsepdfs.sh
@@ -4,7 +4,7 @@ SCRIPTDIR=$(dirname $0)
 # Download the binaries (if not yet existing)
 VERSION=3.3.1
 for lib in core javase; do
-  [ -e $SCRIPTDIR/zx-$lib.jar ] || wget http://repo1.maven.org/maven2/com/google/zxing/$lib/$VERSION/$lib-$VERSION.jar -O $SCRIPTDIR/zx-$lib.jar
+  [ -e $SCRIPTDIR/zx-$lib.jar ] || wget https://repo1.maven.org/maven2/com/google/zxing/$lib/$VERSION/$lib-$VERSION.jar -O $SCRIPTDIR/zx-$lib.jar
 done
 [ -e $SCRIPTDIR/jcommander.jar ] || wget https://repo1.maven.org/maven2/com/beust/jcommander/1.72/jcommander-1.72.jar -O $SCRIPTDIR/jcommander.jar
 
